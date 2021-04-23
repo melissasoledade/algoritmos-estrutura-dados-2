bafd4f6f5d892ac9b298cf6301120c8484dc14ee
Melissa Soledade <30574287+melissasoledade@users.noreply.github.com>
2021-04-22 22:17:33 -0300

    Add files via upload

--- kruskal2.cpp
+++ kruskal2.cpp
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Edge{
-	int u;
-	int v;
-	int peso;
-};typedef struct Edge Edge;
+struct Aresta{
+	int u; //fonte
+	int v; //destino
+	float peso; //peso -> custo gasolina + pedágio
+};typedef struct Aresta Aresta;
 
 struct Grafo{
 	int V;
 	int E;
-	Edge * edge;
+	Aresta * aresta;
 };typedef struct Grafo Grafo;
 
 struct subset{
@@ -22,11 +22,11 @@ struct subset{
 
 
 // Cria grafo com V vértices e E arestas
-Grafo* criaGrafo(int V, int E){
-	Grafo* grafo = (Grafo*)malloc(sizeof(Grafo));
+Grafo* criaGrafo(int V, int E){	
+	Grafo *grafo = (Grafo*)malloc(sizeof(Grafo));	
+	grafo->aresta = (Aresta*)malloc(E * sizeof(Aresta));
 	grafo->V = V;
 	grafo->E = E;
-	grafo->edge = (Edge*)malloc(E * sizeof(Edge));
 
 	return grafo;
 }
@@ -70,55 +70,39 @@ void Union(subset subconjuntos[], int x, int y){
 	}	
 }
 
-// compara Edges pelo peso
+// compara arestas pelo peso
 int compara(const void* x, const void* y){	
-	Edge* i = (Edge*)x;
-	Edge* j = (Edge*)y;	
+	Aresta* i = (Aresta*)x;
+	Aresta* j = (Aresta*)y;	
 	return i->peso > j->peso;	
 }
 
 void Kruskal(Grafo* grafo){
-	int V = grafo->V;
-	Edge resultado[V]; // Tnis will store the resultadoant MST
-	int e = 0; // An index variable, used for resultado[]
-	int i = 0; // An index variable, used for sorted edges
-
-	
-	qsort(grafo->edge, grafo->E, sizeof(grafo->edge[0]), compara);
+	int V = grafo->V, e = 0, i = 0;
+	float custoMin = 0.00;
+	Aresta resultado[V]; 	
+	qsort(grafo->aresta, grafo->E, sizeof(grafo->aresta[0]), compara);
 	subset* subsets = Make_Subset(V);
-
-	for (int v = 0; v < V; ++v)
-	{
-		subsets[v].pai = v;
-		subsets[v].rank = 0;
-	}
-
-	
-	while (e < V - 1 && i < grafo->E)
-	{
 		
-		Edge prox = grafo->edge[i++];
-
+	while (e < V - 1 && i < grafo->E){
+		
+		Aresta prox = grafo->aresta[i++];
 		int x = Find_Set(subsets, prox.u);
 		int y = Find_Set(subsets, prox.v);
-
 		
 		if (x != y) {
 			resultado[e++] = prox;
 			Union(subsets, x, y);
 		}
 		
-	}
-
+	}	
 	
-
-	int custoMin = 0;
 	for (i = 0; i < e; ++i){
-		printf("%d - %d - %d\n", resultado[i].u, resultado[i].v, resultado[i].peso);		
+		printf("Fonte: %d - Destino: %d - Peso: %.2f\n", resultado[i].u, resultado[i].v, resultado[i].peso);		
 		custoMin = custoMin + resultado[i].peso;
 	}
 	
-	printf("Custo mínimo: %d\n", custoMin);
+	printf("Custo mínimo: %.2f\n", custoMin);
 }
 
 
@@ -128,36 +112,35 @@ int main(){
 	int E = 5; 
 	Grafo* grafo = criaGrafo(V, E);
 
-	// add edge 0-1
-	grafo->edge[0].u = 0;
-	grafo->edge[0].v = 1;
-	grafo->edge[0].peso = 10;
+	// add aresta 0-1
+	grafo->aresta[0].u = 0;
+	grafo->aresta[0].v = 1;
+	grafo->aresta[0].peso = 10;
 
-	// add edge 0-2
-	grafo->edge[1].u = 0;
-	grafo->edge[1].v = 2;
-	grafo->edge[1].peso = 6;
+	// add aresta 0-2
+	grafo->aresta[1].u = 0;
+	grafo->aresta[1].v = 2;
+	grafo->aresta[1].peso = 6;
 
-	// add edge 0-3
-	grafo->edge[2].u = 0;
-	grafo->edge[2].v = 3;
-	grafo->edge[2].peso = 5;
+	// add aresta 0-3
+	grafo->aresta[2].u = 0;
+	grafo->aresta[2].v = 3;
+	grafo->aresta[2].peso = 5;
 
-	// add edge 1-3
-	grafo->edge[3].u = 1;
-	grafo->edge[3].v = 3;
-	grafo->edge[3].peso = 15;
+	// add aresta 1-3
+	grafo->aresta[3].u = 1;
+	grafo->aresta[3].v = 3;
+	grafo->aresta[3].peso = 15;
 
-	// add edge 2-3
-	grafo->edge[4].u = 2;
-	grafo->edge[4].v = 3;
-	grafo->edge[4].peso = 4;
+	// add aresta 2-3
+	grafo->aresta[4].u = 2;
+	grafo->aresta[4].v = 3;
+	grafo->aresta[4].peso = 4;
 
 
-	// Function call
+
 	Kruskal(grafo);
 
 	return 0;
 }
 
-// This code is contributed by rathbhupendra
